c64ff72fd6e4b9aa05092d40af312e1ddc92f5eb
Rajdeep Datta <rajdeepdatta2016@gmail.com>
2024-03-13 04:03:20 +0530

    Project Starts

---
+++ api/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = '3iv2r43griu43rv4i3rvi34rvi34v';
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+}));
+
+ mongoose.connect(process.env.MONGO_URL);
+
+app.get('/test', (req,res) =>  {
+    res.json('test ok');
+});
+
+app.post('/register', async (req,res) => {
+    const {name,email,password} = req.body;
+
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch(e) {
+        res.status(422).json(e);
+    }
+});
+
+app.post('/login', async (req,res) => {
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email}); 
+    if(userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if(passOk) {
+            jwt.sign({
+                email:userDoc.email, 
+                id:userDoc._id
+            }, jwtSecret, {}, (err,token) => {
+                if(err) throw err;
+                res.cookie('token', token).json(userDoc);
+            });
+        } else {
+            res.json('pass not ok');
+        }
+    } else {
+        res.json('not found');
+    }
+});
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    if(token) {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            if(err) throw err;
+            const {name,email,_id} = await User.findById(userData.id);
+            res.json({name,email,_id});
+        });
+    } else {
+        res.json(null);
+    }
+});
+
+app.listen(4000);
